7bcd2b9ff98799e20626be3879c24dd945ae4cfa
Ahmed Harris <24aitc@gmail.com>
2025-08-03 23:23:36 -0400

    Update node-group-iam.tf

--- node-group-iam.tf
+++ node-group-iam.tf
@@ -7,8 +7,8 @@ resource "aws_eks_node_group" "my_node_group" {
 
 
 
-resource "aws_iam_role" "node-group-role" {
-  name = "node-group-nodes"
+resource "aws_iam_role" "eks_node_group_role" {
+  name = "eks_node_group_role"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
@@ -27,18 +27,18 @@ resource "aws_iam_role" "node-group-role" {
 
 # Allows EC2 worker nodes to join the EKS cluster and communicate with control plane
 resource "aws_iam_role_policy_attachment" "nodes_AmazonEKSWorkerNodePolicy" {
-  role       = aws_iam_role.node-group-role.name
+  role       = aws_iam_role.eks_node_group_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
 }
 
 # Allows networking capabilities using the Amazon VPC CNI plugin
 resource "aws_iam_role_policy_attachment" "nodes_AmazonEKS_CNI_Policy" {
-  role       = aws_iam_role.node-group-role.name
+  role       = aws_iam_role.eks_node_group_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
 }
 
 # Allows pulling container images from Amazon ECR (Elastic Container Registry)
 resource "aws_iam_role_policy_attachment" "nodes_AmazonEC2ContainerRegistryReadOnly" {
-  role       = aws_iam_role.node-group-role.name
+  role       = aws_iam_role.eks_node_group_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
 }
