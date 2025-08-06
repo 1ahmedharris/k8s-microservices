a0c5546524a2574492f2598f71abcc00bb65efb2
Ahmed Harris <24aitc@gmail.com>
2025-08-06 19:06:35 -0400

    Update vpc.tf

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -13,15 +13,12 @@ module "vpc" {
 
   enable_dns_hostnames = true
   enable_dns_support   = true
+  
   enable_nat_gateway   = true
   single_nat_gateway   = true
 
   enable_dynamodb_endpoint = true
 
-  
-# These will be handled separately (do not include for now)
-  #manage_default_route_table = false
-                   
   tags = {
     "kubernetes.io/cluster/${var.cluster_name}" = "shared"
     "kubernetes.io/role/internal-elb"           = "1"
@@ -29,6 +26,7 @@ module "vpc" {
   }
 }
 }
+
 
 
 
