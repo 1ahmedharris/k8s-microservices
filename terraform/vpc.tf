bd061ee8edcc2458f4a6fd7be1ec82fe609a3c79
Ahmed Harris <24aitc@gmail.com>
2025-08-06 16:40:08 -0400

    Update vpc.tf

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -1,3 +1,8 @@
+vpc_cidr_blocks = "10.0.0.0/16"
+private_subnet_cidr_blocks = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+public_subnet_cidr_blocks = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+
+
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "5.1.0"
@@ -19,3 +24,4 @@ module "vpc" {
     Project     = "eks-resume"
   }
 }
+
