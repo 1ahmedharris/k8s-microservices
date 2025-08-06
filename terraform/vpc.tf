c4706f8b9d6bf83237859c5181f6c47e8bcaeb3a
Ahmed Harris <24aitc@gmail.com>
2025-08-06 17:20:09 -0400

    Update vpc.tf

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -1,27 +1,23 @@
-vpc_cidr_blocks = "10.0.0.0/16"
-private_subnet_cidr_blocks = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-public_subnet_cidr_blocks = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
 
 
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
-  version = "5.1.0"
+  version = "6.0.0" 
+  name    = "eks-vpc"
 
-  name = "eks-vpc"
-  cidr = "10.0.0.0/16"
+  cidr            = var.vpc_cidr_block
+  azs             = ["${var.aws_region}a", "${var.aws_region}b", "${var.aws_region}c"]
+  public_subnets  = var.public_subnet_cidr_blocks
+  private_subnets = var.private_subnet_cidr_blocks
 
-  azs             = ["us-east-1a", "us-east-1b"] # adjust as needed
-  public_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
-  private_subnets = ["10.0.101.0/24", "10.0.102.0/24"]
-
-  enable_nat_gateway   = true
-  single_nat_gateway   = true
   enable_dns_hostnames = true
   enable_dns_support   = true
-
-  tags = {
-    Environment = "dev"
-    Project     = "eks-resume"
-  }
+  enable_nat_gateway   = true
+  single_nat_gateway   = true
+  
+# These will be handled separately (do not include for now)
+  manage_default_route_table = false
+  #create_igw                 = false
+  #create_nat_gateway         = false
+  #single_nat_gateway         = false
 }
-
