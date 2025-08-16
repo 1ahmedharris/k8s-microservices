794b52a31504a090dbacf13730ad6632b4e1ad2d
Ahmed Harris <24aitc@gmail.com>
2025-08-16 00:55:29 -0400

    Update vpc.tf

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -159,6 +159,206 @@ resource "aws_network_acl" "private" {
 
 
 
+data "aws_availability_zones" "azs" {}
+
+# Security group for VPC endpoints
+resource "aws_security_group" "vpc_endpoint" {
+  vpc_id = var.vpc_id
+  name   = "${var.cluster_name}-vpc-endpoint-sg"
+
+  ingress {
+    protocol    = "tcp"
+    from_port   = 443
+    to_port     = 443
+    cidr_blocks = [var.vpc_cidr_block]
+  }
+
+  tags = {
+    Name = "${var.cluster_name}-vpc-endpoint-sg"
+  }
+}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "6.0.0"
+  name    = "eks-vpc"
+
+  cidr            = var.vpc_cidr_block
+  azs             = data.aws_availability_zones.azs.names[0:3]
+  public_subnets  = var.public_subnet_cidr_blocks
+  private_subnets = var.private_subnet_cidr_blocks
+
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+  
+  enable_nat_gateway   = true
+  single_nat_gateway   = true
+
+  enable_dynamodb_endpoint = true
+
+  # Interface VPC Endpoints
+  create_vpc_endpoint_interface_endpoints = true
+  interface_endpoints = {
+    cloudwatch = {
+      service_name        = "com.amazonaws.${var.region}.monitoring"
+      subnet_ids          = module.vpc.private_subnets
+      security_group_ids  = [aws_security_group.vpc_endpoint.id]
+      private_dns_enabled = true
+      tags                = { Name = "cloudwatch-endpoint" }
+    },
+    ecr_api = {
+      service_name        = "com.amazonaws.${var.region}.ecr.api"
+      subnet_ids          = module.vpc.private_subnets
+      security_group_ids  = [aws_security_group.vpc_endpoint.id]
+      private_dns_enabled = true
+      tags                = { Name = "ecr-api-endpoint" }
+    },
+    ecr_dkr = {
+      service_name        = "com.amazonaws.${var.region}.ecr.dkr"
+      subnet_ids          = module.vpc.private_subnets
+      security_group_ids  = [aws_security_group.vpc_endpoint.id]
+      private_dns_enabled = true
+      tags                = { Name = "ecr-dkr-endpoint" }
+    },
+    sts = {
+      service_name        = "com.amazonaws.${var.region}.sts"
+      subnet_ids          = module.vpc.private_subnets
+      security_group_ids  = [aws_security_group.vpc_endpoint.id]
+      private_dns_enabled = true
+      tags                = { Name = "sts-endpoint" }
+    }
+  }
+
+  public_subnet_tags = {
+    "kubernetes.io/cluster/${var.cluster_name}" = "shared"
+    "kubernetes.io/role/elb"                    = "1"
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/cluster/${var.cluster_name}" = "shared"
+    "kubernetes.io/role/internal-elb"           = "1"
+  }
+}
+
+# PUBLIC SUBNET NACL
+resource "aws_network_acl" "public" {
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.public_subnets
+
+  # Ingress: Allow HTTPS from CloudFront/internet to ALB
+  ingress {
+    rule_no    = 100
+    protocol   = "tcp"
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 443
+    to_port    = 443
+  }
+
+  # Ingress: Allow return traffic from pods (ephemeral ports)
+  ingress {
+    rule_no    = 110
+    protocol   = "tcp"
+    action     = "allow"
+    cidr_block = var.vpc_cidr_block
+    from_port  = 1024
+    to_port    = 65535
+  }
+
+  # Egress: Allow ALB to pods (HTTP:80)
+  egress {
+    rule_no    = 100
+    protocol   = "tcp"
+    action     = "allow"
+    cidr_block = var.vpc_cidr_block
+    from_port  = 80
+    to_port    = 80
+  }
+
+  # Egress: Allow ALB responses to CloudFront (ephemeral ports)
+  egress {
+    rule_no    = 110
+    protocol   = "tcp"
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 1024
+    to_port    = 65535
+  }
+
+  tags = {
+    Name = "public-nacl"
+  }
+}
+
+# PRIVATE SUBNET NACL
+resource "aws_network_acl" "private" {
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
+
+  # Ingress: Allow ALB to pods (HTTP:80)
+  ingress {
+    rule_no    = 100
+    protocol   = "tcp"
+    action     = "allow"
+    cidr_block = var.vpc_cidr_block
+    from_port  = 80
+    to_port    = 80
+  }
+
+  # Ingress: Allow EKS control plane kubelet (10250)
+  ingress {
+    rule_no    = 110
+    protocol   = "tcp"
+    action     = "allow"
+    cidr_block = var.vpc_cidr_block
+    from_port  = 10250
+    to_port    = 10250
+  }
+
+  # Ingress: Allow EKS control plane (HTTPS:443)
+  ingress {
+    rule_no    = 120
+    protocol   = "tcp"
+    action     = "allow"
+    cidr_block = var.vpc_cidr_block
+    from_port  = 443
+    to_port    = 443
+  }
+
+  # Ingress: Allow return traffic from ALB/AWS services (ephemeral ports)
+  ingress {
+    rule_no    = 130
+    protocol   = "tcp"
+    action     = "allow"
+    cidr_block = var.vpc_cidr_block
+    from_port  = 1024
+    to_port    = 65535
+  }
+
+  # Egress: Allow pod responses to ALB (ephemeral ports)
+  egress {
+    rule_no    = 100
+    protocol   = "tcp"
+    action     = "allow"
+    cidr_block = var.vpc_cidr_block
+    from_port  = 1024
+    to_port    = 65535
+  }
+
+  # Egress: Allow to EKS control plane/DynamoDB/CloudWatch/ECR/STS endpoints (HTTPS:443)
+  egress {
+    rule_no    = 110
+    protocol   = "tcp"
+    action     = "allow"
+    cidr_block = var.vpc_cidr_block
+    from_port  = 443
+    to_port    = 443
+  }
+
+  tags = {
+    Name = "private-nacl"
+  }
+}
 
 
 
