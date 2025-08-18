82d4af649883d74b94fd064c03fef4a5e808d58b
Ahmed Harris <24aitc@gmail.com>
2025-08-17 21:36:23 -0400

    fix: nacl rules for aws services & updates

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -1,4 +1,4 @@
-# v3
+# v4
 
 
 data "aws_availability_zones" "azs" {}
@@ -20,7 +20,7 @@ module "vpc" {
   single_nat_gateway   = true
 
   enable_dynamodb_endpoint = true
-  enable_s3_endpoint = true
+
 
   public_subnet_tags = {
     "kubernetes.io/cluster/${var.cluster_name}" = "shared"
@@ -33,203 +33,6 @@ module "vpc" {
   }
 }
 
-
-# PUBLIC SUBNET NACL
-resource "aws_network_acl" "public" {
-  vpc_id     = module.vpc.vpc_id
-  subnet_ids = module.vpc.public_subnets
-
-  # Ingress: Allow HTTPS
-  ingress {
-    rule_no    = 100
-    protocol   = "tcp"
-    action     = "allow"
-    cidr_block = "0.0.0.0/0"
-    from_port  = 443
-    to_port    = 443
-  }
-
-  # Ingress: Allow internal vpc traffic
-  ingress {
-    rule_no    = 110
-    protocol   = "tcp"
-    action     = "allow"
-    cidr_block = var.vpc_cidr_block
-    from_port  = 1024
-    to_port    = 65535
-  }
-
-
-  # Egress: Allow HTTP to worker nodes
-  egress {
-    rule_no    = 100
-    protocol   = "tcp"
-    action     = "allow"
-    cidr_block = var.vpc_cidr_block
-    from_port  = 80
-    to_port    = 80
-  }
-
-  # Egress: Allow outbound traffic to client 
-  egress {
-    rule_no    = 110
-    protocol   = "tcp"
-    action     = "allow"
-    cidr_block = "0.0.0.0/0"
-    from_port  = 1024
-    to_port    = 65535
-  }
-
-  tags = {
-    Name = "public-nacl"
-  }
-}
-
-
-# PRIVATE SUBNET NACL
-resource "aws_network_acl" "private" {
-  vpc_id     = module.vpc.vpc_id
-  subnet_ids = module.vpc.private_subnets
-
-  # Ingress: Allow HTTP from VPC
-  ingress {
-    rule_no    = 100
-    protocol   = "tcp"
-    action     = "allow"
-    cidr_block = var.vpc_cidr_block
-    from_port  = 80
-    to_port    = 80
-  }
-
-  ingress {
-    rule_no    = 120
-    protocol   = "tcp"
-    action     = "allow"
-    cidr_block = vpc_cidr_block
-    from_port  = 443
-    to_port    = 443
-  }
-
-  ingress {
-    rule_no    = 110
-    protocol   = "tcp"
-    action     = "allow"
-    cidr_block = vpc_cidr_block
-    from_port  = 1024
-    to_port    = 65535
-  }
-
-
-
-  # Egress: Allow node responses on ephemeral ports 
-  egress {
-    rule_no    = 100
-    protocol   = "tcp"
-    action     = "allow"
-    cidr_block = var.vpc_cidr_block
-    from_port  = 1024
-    to_port    = 65535
-  }
-
-  egress {
-    rule_no    = 110
-    protocol   = "tcp"
-    action     = "allow"
-    cidr_block = var.vpc_cidr_block
-    from_port  = 443
-    to_port    = 443
-  }
-
-
-  tags = {
-    Name = "private-nacl"
-  }
-}
-
-
-
-
-
-data "aws_availability_zones" "azs" {}
-
-# Security group for VPC endpoints
-resource "aws_security_group" "vpc_endpoint" {
-  vpc_id = var.vpc_id
-  name   = "${var.cluster_name}-vpc-endpoint-sg"
-
-  ingress {
-    protocol    = "tcp"
-    from_port   = 443
-    to_port     = 443
-    cidr_blocks = [var.vpc_cidr_block]
-  }
-
-  tags = {
-    Name = "${var.cluster_name}-vpc-endpoint-sg"
-  }
-}
-
-module "vpc" {
-  source  = "terraform-aws-modules/vpc/aws"
-  version = "6.0.0"
-  name    = "eks-vpc"
-
-  cidr            = var.vpc_cidr_block
-  azs             = data.aws_availability_zones.azs.names[0:3]
-  public_subnets  = var.public_subnet_cidr_blocks
-  private_subnets = var.private_subnet_cidr_blocks
-
-  enable_dns_hostnames = true
-  enable_dns_support   = true
-  
-  enable_nat_gateway   = true
-  single_nat_gateway   = true
-
-  enable_dynamodb_endpoint = true
-
-  # Interface VPC Endpoints
-  create_vpc_endpoint_interface_endpoints = true
-  interface_endpoints = {
-    cloudwatch = {
-      service_name        = "com.amazonaws.${var.region}.monitoring"
-      subnet_ids          = module.vpc.private_subnets
-      security_group_ids  = [aws_security_group.vpc_endpoint.id]
-      private_dns_enabled = true
-      tags                = { Name = "cloudwatch-endpoint" }
-    },
-    ecr_api = {
-      service_name        = "com.amazonaws.${var.region}.ecr.api"
-      subnet_ids          = module.vpc.private_subnets
-      security_group_ids  = [aws_security_group.vpc_endpoint.id]
-      private_dns_enabled = true
-      tags                = { Name = "ecr-api-endpoint" }
-    },
-    ecr_dkr = {
-      service_name        = "com.amazonaws.${var.region}.ecr.dkr"
-      subnet_ids          = module.vpc.private_subnets
-      security_group_ids  = [aws_security_group.vpc_endpoint.id]
-      private_dns_enabled = true
-      tags                = { Name = "ecr-dkr-endpoint" }
-    },
-    sts = {
-      service_name        = "com.amazonaws.${var.region}.sts"
-      subnet_ids          = module.vpc.private_subnets
-      security_group_ids  = [aws_security_group.vpc_endpoint.id]
-      private_dns_enabled = true
-      tags                = { Name = "sts-endpoint" }
-    }
-  }
-
-  public_subnet_tags = {
-    "kubernetes.io/cluster/${var.cluster_name}" = "shared"
-    "kubernetes.io/role/elb"                    = "1"
-  }
-
-  private_subnet_tags = {
-    "kubernetes.io/cluster/${var.cluster_name}" = "shared"
-    "kubernetes.io/role/internal-elb"           = "1"
-  }
-}
 
 # PUBLIC SUBNET NACL
 resource "aws_network_acl" "public" {
@@ -246,12 +49,21 @@ resource "aws_network_acl" "public" {
     to_port    = 443
   }
 
-  # Ingress: Allow return traffic from pods (ephemeral ports)
+  # Ingress: Allow traffic from aws services/updates on ephemeral ports
   ingress {
     rule_no    = 110
     protocol   = "tcp"
     action     = "allow"
-    cidr_block = var.vpc_cidr_block
+    cidr_block = "0.0.0.0/0"
+    from_port  = 1024
+    to_port    = 65535
+  }
+
+  ingress {
+    rule_no    = 110
+    protocol   = "tcp"
+    action     = "allow"
+    cidr_block = "var.vpc_cidr_block
     from_port  = 1024
     to_port    = 65535
   }
@@ -266,7 +78,7 @@ resource "aws_network_acl" "public" {
     to_port    = 80
   }
 
-  # Egress: Allow ALB responses to CloudFront (ephemeral ports)
+  # Egress: Allow responses/requests to internet on ephemeral ports
   egress {
     rule_no    = 110
     protocol   = "tcp"
@@ -340,6 +152,7 @@ resource "aws_network_acl" "private" {
     Name = "private-nacl"
   }
 }
+
 
 
 
