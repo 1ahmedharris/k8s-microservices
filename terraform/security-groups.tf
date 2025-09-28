b587a7a9f8fd4bc0e33db104621c0e4c5aab41b1
Ahmed Harris <24aitc@gmail.com>
2025-09-28 04:33:08 -0400

    refactor: defining sg modules seperate from sg rules

--- terraform/security-groups.tf
+++ terraform/security-groups.tf
@@ -1,141 +1,32 @@
 # EKS Managed Node Group Security Group
 module "node_group_sg" {
-  source  = "terraform-aws-modules/security-group/aws"
-  version = "5.2.0"
+  source          = "terraform-aws-modules/security-group/aws"
+  version         = "~> 5.3.0"
 
-  name        = "${var.cluster_name}-node-group-sg"
-  description = "Security group for EKS managed node group"
-  vpc_id      = module.vpc.vpc_id
-
-  ingress_with_source_security_group_id = [
-    {
-      description              = "Allow HTTP from ALB"
-      from_port                = 80
-      to_port                  = 80
-      protocol                 = "tcp"
-      source_security_group_id = module.alb_sg.security_group_id
-    },
-    {
-      description              = "Allow HTTPS from EKS control plane"
-      from_port                = 443
-      to_port                  = 443
-      protocol                 = "tcp"
-      source_security_group_id = module.cluster_sg.security_group_id
-    },
-    {
-      description              = "Allow kubelet from EKS control plane"
-      from_port                = 10250
-      to_port                  = 10250
-      protocol                 = "tcp"
-      source_security_group_id = module.cluster_sg.security_group_id
-    }
-  ]
-
-  egress_with_source_security_group_id = [
-    {
-      description              = "Allow HTTP to ALB"
-      from_port                = 80
-      to_port                  = 80
-      protocol                 = "tcp"
-      source_security_group_id = module.alb_sg.security_group_id
-    },
-    {
-      description              = "Allow HTTPS to EKS control plane"
-      from_port                = 443
-      to_port                  = 443
-      protocol                 = "tcp"
-      source_security_group_id = module.cluster_sg.security_group_id
-    }
-  ]
-
-  egress_with_cidr_blocks = [
-    {
-      description = "Allow HTTPS to NAT Gateway for ECR/updates"
-      from_port   = 443
-      to_port     = 443
-      protocol    = "tcp"
-      cidr_blocks = ["0.0.0.0/0"]
-    }
-  ]
-
-  tags = {
-    Name = "${var.cluster_name}-node-group-sg"
-  }
+  name            = "${var.cluster_name}-node-group-sg"
+  description     = "Security group for EKS managed node group"
+  vpc_id          = module.vpc.vpc_id
 }
 
 
 # ALB Security Group
 module "alb_sg" {
-  source  = "terraform-aws-modules/security-group/aws"
-  version = "5.2.0"
+  source          = "terraform-aws-modules/security-group/aws"
+  version         = "~> 5.3.0"
 
-  name        = "${var.cluster_name}-alb-sg"
-  description = "Security group for ALB"
-  vpc_id      = module.vpc.vpc_id
-
-  ingress_with_cidr_blocks = [
-    {
-      description = "Allow HTTPS from CloudFront"
-      from_port   = 443
-      to_port     = 443
-      protocol    = "tcp"
-      cidr_blocks = ["0.0.0.0/0"]
-    }
-  ]
-
-  egress_with_source_security_group_id = [
-    {
-      description              = "Allow HTTP to EKS nodes"
-      from_port                = 80
-      to_port                  = 80
-      protocol                 = "tcp"
-      source_security_group_id = module.node_group_sg.security_group_id
-    }
-  ]
-
-  tags = {
-    Name = "${var.cluster_name}-alb-sg"
-  }
+  name            = "${var.cluster_name}-alb-sg"
+  description     = "Security group for alb"
+  vpc_id          = module.vpc.vpc_id
 }
 
 
-# EKS Cluster Control Plane Security Group
+# Cluster Security Group
 module "cluster_sg" {
-  source  = "terraform-aws-modules/security-group/aws"
-  version = "5.2.0"
+  source          = "terraform-aws-modules/security-group/aws"
+  version         = "~> 5.3.0"
 
-  name        = "${var.cluster_name}-cluster-sg"
-  description = "Security group for EKS cluster control plane"
-  vpc_id      = module.vpc.vpc_id
-
-  ingress_with_source_security_group_id = [
-    {
-      description              = "Allow HTTPS from EKS nodes"
-      from_port                = 443
-      to_port                  = 443
-      protocol                 = "tcp"
-      source_security_group_id = module.node_group_sg.security_group_id
-    }
-  ]
-
-  egress_with_source_security_group_id = [
-    {
-      description              = "Allow HTTPS to EKS nodes"
-      from_port                = 443
-      to_port                  = 443
-      protocol                 = "tcp"
-      source_security_group_id = module.node_group_sg.security_group_id
-    },
-    {
-      description              = "Allow kubelet to EKS nodes"
-      from_port                = 10250
-      to_port                  = 10250
-      protocol                 = "tcp"
-      source_security_group_id = module.node_group_sg.security_group_id
-    }
-  ]
-
-  tags = {
-    Name = "${var.cluster_name}-cluster-sg"
-  }
+  name            = "${var.cluster_name}-cluster-sg"
+  description     = "Security group for alb"
+  vpc_id          = module.vpc.vpc_id
 }
+
