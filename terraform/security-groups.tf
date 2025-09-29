1bb2592e08ac59c5b5394a8225f60ee46d56c709
Ahmed Harris <24aitc@gmail.com>
2025-09-29 19:42:08 -0400

    feat: added ingress rules node_sg

--- terraform/security-groups.tf
+++ terraform/security-groups.tf
@@ -12,3 +12,16 @@ resource "aws_security_group" "alb_sg" {
   description     = "Security group for alb"
   vpc_id          = module.vpc.vpc_id
 }
+
+
+resource "aws_vpc_security_group_ingress_rule" "allow_http" {
+  security_group_id = aws_security_group.node_sg.id
+  description       = "Allow HTTP traffic from alb"
+  from_port         = 80
+  to_port           = 80
+  ip_protocol       = "tcp"
+  cidr_ipv4         = "10.0.0.0/20" # Allows traffic from vpc
+}
+
+
+
